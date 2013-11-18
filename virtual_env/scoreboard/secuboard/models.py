553f81f481c07cb21562485ba8589159c6fceb7b
Francois Serman <Francois.Serman@inria.fr>
2013-11-19 00:37:14 +0100

    adding linux branch

---
+++ virtual_env/scoreboard/secuboard/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
+
+class Challenge(models.Model):
+	starts_at   = models.DateTimeField()
+	ends_at     = models.DateTimeField()
+	title       = models.CharField(max_length=200)
+	challenge   = models.CharField(max_length=2000)
+	answer      = models.CharField(max_length=50)
+        reward      = models.IntegerField()
+        bonus       = models.IntegerField(default=0)
+	linked_file = models.FileField(upload_to='attached_files', null=True, blank=True)
+
+	def __unicode__(self):
+		return self.title
+
+class Response(models.Model):
+	user      = models.ForeignKey(User,)
+	challenge = models.ForeignKey(Challenge)
+	date      = models.DateTimeField()
+
+	def __unicode__(self):
+		return '%s at %s' % (self.user, self.date)
+
+class ChallengeAdmin(admin.ModelAdmin):
+    pass
+
+class ResponseAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Challenge, ChallengeAdmin)
+admin.site.register(Response, ResponseAdmin)
+	
